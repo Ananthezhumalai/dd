794803b1f045b6c1c5dfac9e557613a4e889ff93
Ananthezhumalai <ananthezhumalai@gmail.com>
2025-03-24 11:11:16 +0530

    Updated Node.js version for Vercel

--- material-ui-portfolio/server/server.js
+++ material-ui-portfolio/server/server.js
@@ -6,7 +6,6 @@ require("dotenv").config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-   
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
